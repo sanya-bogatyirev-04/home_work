f998c45572c9ea31fb575548854ce9d30c1c140e
sanya-bogatyirev-04 <sanya.bogatyirev.04@mail.ru>
2025-04-12 18:22:51 +0300

    update enter

--- work.js
+++ work.js
@@ -1,8 +1,11 @@
 function check() {
   if (
     document.getElementById("password_repeat").value ===
-    document.getElementById("password").value
-  ) {
-    return true;
-  } else { return false; }
-}
+    document.getElementById("password").value && document.getElementById("password").value != ""
+  ) { document.getElementById("enter").disabled = 0
+
+  } else {
+      document.getElementById("enter").disabled = 1
+    };
+  }
+
