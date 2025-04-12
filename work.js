e5664f6147cf4fd2a59e7825e2060ce1f2a48063
sanya-bogatyirev-04 <sanya.bogatyirev.04@mail.ru>
2025-04-12 17:24:20 +0300

    add js code

---
+++ work.js
@@ -0,0 +1,10 @@
+function check() {
+  if (
+    document.getElementById("password_repeat").value ===
+    document.getElementById("password").value
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
